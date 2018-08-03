1113b0e82cfb6f0d2c3f69ac7ace63f0047a1ad9
Jose Luis Martinez <jlmartinez@capside.com>
2018-08-03 09:03:30 +0000

    Documentation

--- lib/GCloud/CLIWrapper.pm
+++ lib/GCloud/CLIWrapper.pm
@@ -66,3 +66,93 @@ package GCloud::CLIWrapper;
   }
 
 1;
+### main pod documentation begin ###
+ 
+=encoding UTF-8
+ 
+=head1 NAME
+ 
+GCloud::CLIWrapper - Module to use Google Cloud APIs via the gcloud CLI
+ 
+=head1 SYNOPSIS
+ 
+  use GCloud::CLIWrapper;
+ 
+  my $api = GCloud::CLIWrapper->new();
+ 
+  my $result = $api->run('info');
+  # $result->success == 1 if the command executed correctly
+  # $result->output contains the output of the command
+ 
+  my $result = $api->json('info', '--format', 'json');
+  # $result->success == 1 if the command executed correctly
+  # $result->output contains the output of the command
+  # $result->json is a hashref with the result of the parsed JSON output of the command
+ 
+=head1 DESCRIPTION
+ 
+This module helps you use the GCloud API. It sends all it's commands
+via the CLI command line tool C<gcloud>. 
+ 
+=head1 ATTRIBUTES
+ 
+=head2 glcloud
+ 
+By default initialized to C<gcloud>. It will try to find kubectl in the PATH. You can
+set it explicitly to a specific gcloud excecutable.
+ 
+=head2 gcloud_options
+ 
+An ArrayRef of options to always add to the command line invocations.
+ 
+  my $api = GCloud::CLIWrapper->new(
+    gcloud_options => [ 'info' ],
+  );
+ 
+  my $result = $api->run;
+  # $result->success == 1 if the command executed correctly
+  # $result->output contains the output of the command
+ 
+  my $result = $api->json('--format', 'json');
+  # $result->success == 1 if the command executed correctly
+  # $result->output contains the output of the command
+  # $result->json is a hashref with the result of the parsed JSON output of the command
+ 
+=head1 METHODS
+ 
+=head2 run(@parameters)
+ 
+Will run gcloud with the parameters. Returns a L<GCloud::CLIWrapper::Result> object
+with C<output> set to the output of the command, and C<success> a Boolean to indicate
+if the command reported successful execution.
+ 
+=head2 json(@parameters)
+ 
+Will run gcloud with the parameters, trying to parse the output as json. Note that you are
+responsible for passing the command-line option to output in a json format. Returns a L<Kubectl::CLIWrapper::Result> object
+with C<output> set to the output of the command, and C<json> set to a hashref with the parsed
+JSON. C<success> will be false if JSON parsing fails.
+ 
+=head1 SEE ALSO
+ 
+L<https://cloud.google.com/sdk/gcloud/>
+ 
+=head1 AUTHOR
+ 
+    Jose Luis Martinez
+    CAPSiDE
+    jlmartinez@capside.com
+ 
+=head1 BUGS and SOURCE
+ 
+The source code is located here: L<>
+ 
+Please report bugs to: L<>
+ 
+=head1 COPYRIGHT and LICENSE
+ 
+Copyright (c) 2018 by CAPSiDE
+This code is distributed under the Apache 2 License. The full text of the 
+license can be found in the LICENSE file included with this module.
+ 
+=cut
